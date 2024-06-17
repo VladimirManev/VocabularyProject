f67c184567ee13edc1767d3801cbcac494f53e4f
VladimirManev <145099305+VladimirManev@users.noreply.github.com>
2024-06-17 21:05:08 +0200

    Adds unknownSentencesSorter in util.js

--- client/src/services/util.js
+++ client/src/services/util.js
@@ -1,22 +1,31 @@
 export function getUserData() {
-    return JSON.parse(sessionStorage.getItem('userData'));
+  return JSON.parse(sessionStorage.getItem("userData"));
 }
 
 export function setUserData(data) {
-    sessionStorage.setItem('userData', JSON.stringify(data));
+  sessionStorage.setItem("userData", JSON.stringify(data));
 }
 
 export function clearUserData() {
-    sessionStorage.removeItem('userData');
+  sessionStorage.removeItem("userData");
 }
 
 export function createSubmitHandler(callback) {
-    return function (event) {
-        event.preventDefault();
-        const form = event.currentTarget;
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
+  return function (event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
 
-        callback(data, form);
-    }
+    callback(data, form);
+  };
+}
+
+export function unknownSentencesSorter(allSentances, knownSentences) {
+  if (allSentances.length !== 0 && knownSentences.length !== 0) {
+    return allSentances.filter((x) =>
+      knownSentences.every((y) => y.id !== x._id)
+    );
+  }
+  return [];
 }
